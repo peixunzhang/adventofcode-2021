0fc85842294ed210c25d0b17faea4a686fefb550
Peixun Zhang <max317backup@gmail.com>
2021-12-15 21:07:17 +0100

    day13

--- day12/src/main/scala/example/Hello.scala
+++ day12/src/main/scala/example/Hello.scala
@@ -1,9 +1,86 @@
 package example
+import Hello.Path
+import better.files.Resource
+object Hello extends App {
+  type Path = List[Cave]
+  def parseCave(str: String): Cave = str match {
+    case "start" => Cave.Start
+    case "end"               => Cave.End
+    case s if s.head.isLower => Cave.Small(s)
+    case s                   => Cave.Big(s)
+  }
+  def parseData(input: String): List[(Cave, Cave)] = {
+    input
+      .split("\n")
+      .toList
+      .map { str =>
+        str.split("-").toList match {
+          case start :: end :: Nil =>
+            val startCave = parseCave(start)
+            val endCave = parseCave(end)
+            (startCave, endCave)
+          case _ =>
+            throw new IllegalArgumentException()
+        }
+      }
+      .toList
+  }
+  def makeMap(connections: List[(Cave, Cave)]): CaveMap = {
 
-object Hello extends Greeting with App {
-  println(greeting)
+    connections.foldLeft(CaveMap.empty)((a, b) => a.addConnection(b._1, b._2))
+  }
+  println(
+    makeMap(parseData(Resource.getAsString("input.txt")))
+      .findPaths(Cave.Start)
+      .length
+  )
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+final case class CaveMap(connections: Map[Cave, Set[Cave]]) {
+  def findPaths(
+      startPoint: Cave,
+      visitedCaves: Set[Cave] = Set.empty,
+      specialCave: Boolean = false
+  ): List[Path] = startPoint match {
+    case Cave.End => 
+      List(List(Cave.End))
+    case _ =>
+      val newVisited = startPoint match {
+        case Cave.Small(_) => visitedCaves + startPoint
+        case _             => visitedCaves
+      }
+      connections(startPoint).toList
+        .flatMap {
+          case Cave.Start => 
+            Nil
+          case c @ Cave.Small(_) =>
+            val visited = newVisited.contains(c)
+            if (!visited) 
+              findPaths(c, newVisited, specialCave)
+            else if (visited && !specialCave)
+              findPaths(c, newVisited, true)
+            else
+              Nil
+          case c =>
+            findPaths(c, newVisited, specialCave)
+        }
+        .map(startPoint :: _)
+  }
+  def addConnection(start: Cave, end: Cave): CaveMap = {
+    CaveMap(
+      connections
+        + ((start, connections.getOrElse(start, Set.empty) + end))
+        + ((end, connections.getOrElse(end, Set.empty) + start))
+    )
+  }
+}
+object CaveMap {
+  val empty = CaveMap(Map.empty)
+}
+sealed trait Cave
+object Cave {
+  final case class Big(name: String) extends Cave
+  final case class Small(name: String) extends Cave
+  case object Start extends Cave
+  case object End extends Cave
 }
---
+++ day13/src/main/scala/example/Hello.scala
@@ -0,0 +1,94 @@
+package example
+
+import better.files.Resource
+
+object Hello extends App {
+  def parseData(input: String): Board = {
+    val entries = input.split("\n").map{str => str.split(",").toList match {
+      case rs :: cs :: Nil => (rs.toInt, cs.toInt)
+      case _ => throw new IllegalStateException()
+    }}.toSet
+    val initial = Board.empty(1311, 895)
+    initial.updateAll((r, c) => entries.contains((r, c)))
+  }
+
+  val board = 
+    parseData(Resource.getAsString("input.txt"))
+      .foldX
+      .foldY
+      .foldX
+      .foldY
+      .foldX
+      .foldY
+      .foldX
+      .foldY
+      .foldX
+      .foldY
+      .foldY
+      .foldY
+
+  println(board)
+}
+
+final case class Board(value: Vector[Vector[Boolean]], rows: Int, cols: Int) {
+  
+  def getValue(row: Int, col: Int): Boolean = {
+    value(row)(col)
+  }
+
+  def foldX: Board = {
+    println(s"dims: ($rows, $cols)")
+    println(s"foldX: ${rows / 2}")
+    Board.empty(rows / 2, cols).updateAll((r, c) => getValue(r, c) || getValue((rows - 1) - r, c))
+  }
+
+  def foldY: Board = {
+    println(s"dims: ($rows, $cols)")
+    println(s"foldY: ${cols / 2}")
+    Board.empty(rows, cols / 2).updateAll((r, c) => getValue(r ,c) || getValue(r, (cols-1) - c)) 
+  }
+
+  def updateAll(f: (Int, Int) => Boolean): Board = {
+    Board(
+      value.zipWithIndex.map { case (row, rowindex) =>
+        row.zipWithIndex.map { case (_, colindex) =>
+          f(rowindex, colindex)
+        }
+      },
+      rows,
+      cols
+    )
+  }
+  
+  def countMark: Int = {
+    value.flatten.count(identity)
+  }
+  
+  override def toString =
+    value.map(_.map(if (_) 'x' else '.').mkString("")).mkString("\n")
+}
+
+object Board {
+  def empty(rows: Int, cols: Int): Board =
+    Board(Vector.fill(rows, cols)(false), rows, cols)
+}
+
+// get, takes a row, col and turn boolean
+// update row, col, value = board
+// object board empty board x * y to conculate the size of board
+// make a new board conculate new size create new board then update location
+//   0 1 * 3 4 5 6 7 8 9
+//          
+//         1 0            ((fold - 1) - index)
+//         3 4 5 6 7 8 9  ((fold + 1) + index)
+//         0 1 2 3 4 5 6
+//
+//   0 1 2 3 * 5 6 7 8 
+//
+//   0 1 2 3 4 5 6        (index)
+//             9 8        (2 * fold - index)
+//   0 1 2 3 4 5 6
+//
+
+//  0 a b * d e f g h i j
+// row 1 col 10 foldCon 2
