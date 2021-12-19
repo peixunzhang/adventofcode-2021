3e60675141b5374db57a4c1d1a276fe163c8c386
Peixun Zhang <max317backup@gmail.com>
2021-12-19 19:04:27 +0100

    day17

---
+++ day16/src/main/scala/example/Hello.scala
@@ -0,0 +1,114 @@
+package example
+
+import better.files.Resource
+
+object Hello extends App {
+  def loadData: String = {
+    BigInt(Resource.getAsString("input.txt").stripLineEnd, 16).toString(2)
+  }
+
+  def parseMessage(binary: String): (Message, String) = {
+    val version = Integer.parseInt(binary.substring(0, 3), 2)
+    val typeId: Int = Integer.parseInt(binary.substring(3, 6), 2)
+
+    typeId match {
+      case 4 =>
+        val (payload, leftover, _) = binary
+          .drop(6)
+          .grouped(5)
+          .foldLeft((List.empty[String], List.empty[String], false)) {
+            case ((keep, leftover, false), group) =>
+              if (group.head == '1') (group.tail :: keep, leftover, false)
+              else (group.tail :: keep, leftover, true)
+            case ((keep, leftover, true), group) =>
+              (keep, group :: leftover, true)
+          }
+        val body = Literal(
+          java.lang.Long.parseLong(payload.reverse.mkString, 2)
+        )
+        (Message(version, body), leftover.reverse.mkString)
+      case _ =>
+        val lengthTypeId = binary(6).asDigit
+        val (messages, leftOver) = lengthTypeId match {
+          case 0 =>
+            val payloadLength = Integer.parseInt(binary.substring(7, 22), 2)
+            val messagesBinary = binary.substring(22, 22 + payloadLength)
+            val messages = parseManyMessagesNoLeftover(messagesBinary)
+            (messages, binary.drop(22 + payloadLength))
+          case 1 =>
+            val amount = Integer.parseInt(binary.substring(7, 18), 2)
+            val messagesBinary = binary.drop(18)
+            parseNMessages(messagesBinary, amount)
+          case _ =>
+            throw new IllegalStateException()
+        }
+        (Message(version, Operator(typeId, messages)), leftOver)
+    }
+  }
+
+  def parseManyMessagesNoLeftover(messagesBinary: String): List[Message] = {
+    if (messagesBinary.isEmpty()) Nil
+    else {
+      val (message, left) = parseMessage(messagesBinary)
+      message :: parseManyMessagesNoLeftover(left)
+    }
+  }
+
+  def parseNMessages(
+      messagesBinary: String,
+      amount: Int,
+      acc: List[Message] = Nil
+  ): (List[Message], String) = {
+    if (amount <= 0)
+      (acc.reverse, messagesBinary)
+    else {
+      val (message, left) = parseMessage(messagesBinary)
+      parseNMessages(left, amount - 1, message :: acc)
+    }
+  }
+
+  println(parseMessage(loadData)._1.eval)
+}
+
+final case class Message(version: Int, body: MessageBody) {
+  def sumOfVersions: Int =
+    body match {
+      case Literal(_) => version
+      case Operator(_, messages) =>
+        messages.map(_.sumOfVersions).sum + version
+    }
+  def eval: Option[Long] = {
+    body match {
+      case Literal(data) => Some(data)
+      case Operator(typeId, messages) => 
+        val result = messages.flatMap(_.eval)
+        typeId match {
+          case 0 => Some(result.sum)
+          case 1 => Some(result.product)
+          case 2 => result.minOption
+          case 3 => result.maxOption
+          case 5 => result match {
+            case x :: y :: Nil => if (x > y) Some(1) else Some(0)
+            case _ => None 
+          }
+          case 6 => result match {
+            case x :: y :: Nil => if (x < y) Some(1) else Some(0)
+            case _ => None 
+          }
+           case 7 => result match {
+            case x :: y :: Nil => if (x == y) Some(1) else Some(0)
+            case _ => None 
+          }
+          case _ => None
+        }
+    }
+  }
+}
+
+sealed trait MessageBody
+final case class Literal(data: Long) extends MessageBody 
+
+final case class Operator(
+    typeId: Int,
+    messages: List[Message]
+) extends MessageBody 
---
+++ day17/src/main/scala/example/Hello.scala
@@ -0,0 +1,36 @@
+package example
+
+object Hello extends App {
+  def launchAll(target: Target): Int= {
+    val vxMin = 0
+    val vxMax = target.xRight
+    val vyMin = target.yDown
+    val vyMax = -target.yDown
+    val probes = for {
+      vx <- vxMin to vxMax
+      vy <- vyMin to vyMax
+    } yield Probe(0, 0, vx, vy)
+    probes.flatMap(_.launch(target)).size
+  }
+
+  println(launchAll(Target(282, 314, -45, -80)))
+}
+
+final case class Probe(x: Int, y: Int, vx: Int, vy: Int) { self =>
+  def step: Probe = {
+    val newVx = if (vx > 0) vx - 1 else if (vx < 0) vx + 1 else vx
+    Probe(x + vx, y + vy, newVx, vy - 1)
+  }
+  def launch(target: Target, accY: Option[Int] = None): Option[Int] = {
+    if (x > target.xRight || y < target.yDown) None
+    else if (target.isHit(self)) accY.fold(Some(y))(old => Some(old.max(y)))
+    else step.launch(target, accY.fold(Some(y))(old => Some(old.max(y))))
+  }
+  
+}
+
+final case class Target(xLeft: Int, xRight: Int, yUp: Int, yDown: Int) {
+  def isHit(probe: Probe): Boolean =
+    probe.x >= xLeft && probe.x <= xRight && probe.y >= yDown && probe.y <= yUp
+
+}
